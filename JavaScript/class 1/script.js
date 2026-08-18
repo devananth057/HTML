815ca06fcb57e19821918fe0d3b1f7a108b49419
devananth057 <devananth057@gmail.com>
2026-08-18 17:28:17 +0530

    JavaScript class about Datatypes,Scoping

---
+++ JavaScript/class 1/script.js
@@ -0,0 +1,20 @@
+console.log("LOG Notification")
+
+var a=10;
+
+{
+    let a=100;
+    console.log(a)
+}
+
+console.log(a)
+
+var x=1234
+var x="dfghjk"
+var x=undefined
+var x=NaN
+var x=false
+
+
+console.log(typeof(x))
+
